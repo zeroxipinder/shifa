52760b20623b33e0561b4db91d02fd401b8efa37
Saeful Rochim <32793466+dalungid@users.noreply.github.com>
2024-03-09 21:50:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1530,7 +1530,7 @@ if (isset($_GET['tunnel'])) {
 <div class="product-price">
 IDR 0.00
 </div>
-<a href="https://ps4dgacor.xyz/"><img src="https://slot1131.rent/img/Borcelle.gif" style="display: block; margin-left: auto; margin-right: auto;"></a>
+<a href="https://jap88.xyz/"><img src="https://slot1131.rent/img/Borcelle.gif" style="display: block; margin-left: auto; margin-right: auto;"></a>
 
       
         
